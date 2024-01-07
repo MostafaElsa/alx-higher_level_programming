5a8284a84d0d0790e0b7bf12bf0c6225ac59b1d3
MostafaElsa <mostafaelhabashy41@gmail.com>
2024-01-07 15:17:30 +0000

    change permission to excute

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0,10):
-        if i == 9 and j == 9:
-            continue
-        else:
-            print("{}{}".format(i, j), end=', ')
+for i in range(0, 99):
+    print("{:02d}".format(i, j), end=', ')
 print(99)
     
